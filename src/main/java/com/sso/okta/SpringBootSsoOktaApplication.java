697471c74a2c2260e7a55fe4b70046b026560251
Pritam Kumar <pritam.c73@gmail.com>
2023-05-27 22:31:19 +0530

    First COmmit

---
+++ src/main/java/com/sso/okta/SpringBootSsoOktaApplication.java
@@ -0,0 +1,18 @@
+package com.sso.okta;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSsoOktaApplication {
+
+//	public static void main(String[] args) {
+//		String String = "Test";
+//		System.out.println(String);
+//		int int = 0;
+//		System.out.println(int);
+//		Integer Integer = 10;
+//		System.out.println(Integer);
+//		SpringApplication.run(SpringBootSsoOktaApplication.class, args);
+//	}
+
+}
---
+++ src/test/java/com/sso/okta/SpringBootSsoOktaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sso.okta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSsoOktaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
